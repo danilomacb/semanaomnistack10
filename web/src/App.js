646d3ec0975b89d0299da9f28c636a2b8a3c2a3d
danilomacb <danilo.bakun@gmail.com>
2020-01-16 15:27:15 -0300

    aula 03 1:09:00

---
+++ web/src/App.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import "./global.css"
+import "./App.css"
+import "./Sidebar.css"
+import "./Main.css"
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/39657417?s=460&v=4" alt="Danilo Macedo Bakun"/>
+              <div className="user-info">
+                <strong>Danilo Macedo Bakun</strong>
+                <span>ReactJs, Node.JS, Mongo</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Web</p>
+            <a href="https://github.com/danilomacb">Acessar Perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/39657417?s=460&v=4" alt="Danilo Macedo Bakun"/>
+              <div className="user-info">
+                <strong>Danilo Macedo Bakun</strong>
+                <span>ReactJs, Node.JS, Mongo</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Web</p>
+            <a href="https://github.com/danilomacb">Acessar Perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/39657417?s=460&v=4" alt="Danilo Macedo Bakun"/>
+              <div className="user-info">
+                <strong>Danilo Macedo Bakun</strong>
+                <span>ReactJs, Node.JS, Mongo</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Web</p>
+            <a href="https://github.com/danilomacb">Acessar Perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/39657417?s=460&v=4" alt="Danilo Macedo Bakun"/>
+              <div className="user-info">
+                <strong>Danilo Macedo Bakun</strong>
+                <span>ReactJs, Node.JS, Mongo</span>
+              </div>
+            </header>
+            <p>Desenvolvedor Web</p>
+            <a href="https://github.com/danilomacb">Acessar Perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
